4578c3bd299cca093f41f856eea3835409631b0c
codemunki <codemunki@users.noreply.github.com>
2013-11-15 08:50:04 -0500

    Delete runserver.bash

--- minimud/minimud_server/runserver.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export CLASSPATH=/usr/local/lib/mysql-connector-java-5.1.26-bin.jar:.:/home/steve/codemunki-gitrepo/minimud/minimud_shared/dist/minimud_shared.jar:/home/steve/codemunki-gitrepo/minimud/lib/commons-lang3-3.1.jar:.
-
-java -jar ./dist/minimud_server.jar localhost 3306 minimud ntnmfuicd ./minimud_server_log.txt
-
