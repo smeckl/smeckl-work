30f9eebef1af00d35eb66b4c939cd99981e2ca9c
codemunki <codemunki@users.noreply.github.com>
2013-11-15 08:49:07 -0500

    Update buildworld.bash

--- minimud/world_builder/buildworld.bash
+++ minimud/world_builder/buildworld.bash
@@ -2,5 +2,5 @@
 
 export CLASSPATH=/home/steve/codemunki-gitrepo/minimud/lib/mysql-connector-java-5.1.26-bin.jar:.:/home/steve/codemunki-gitrepo/minimud/minimud_shared/dist/minimud_shared.jar
 
-java -jar ./dist/world_builder.jar localhost 3306 minimud ntnmfuicd ../world_data.xml 
+java -jar ./dist/world_builder.jar localhost 3306 minimud password ./world_data.xml 
 
