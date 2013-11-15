7d4bcae5820c052725f86d71e2db4967db445e72
codemunki <codemunki@users.noreply.github.com>
2013-11-15 08:49:43 -0500

    Delete buildworld.bash

--- minimud/world_builder/buildworld.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export CLASSPATH=/home/steve/codemunki-gitrepo/minimud/lib/mysql-connector-java-5.1.26-bin.jar:.:/home/steve/codemunki-gitrepo/minimud/minimud_shared/dist/minimud_shared.jar
-
-java -jar ./dist/world_builder.jar localhost 3306 minimud password ./world_data.xml 
-
