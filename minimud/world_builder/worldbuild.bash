bbe01df8389916e8d4cc231767c2daf37023d960
Steve Meckl <steve@ubuntu.(none)>
2013-11-16 17:28:36 -0500

    Modified minimud server to run with only SELECT, UPDATE, and INSERT privileges.

---
+++ minimud/world_builder/worldbuild.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export CLASSPATH=/home/steve/codemunki-gitrepo/minimud/lib/mysql-connector-java-5.1.26-bin.jar:.:/home/steve/codemunki-gitrepo/minimud/minimud_shared/dist/minimud_shared.jar
+
+java -jar ./dist/world_builder.jar localhost 3306 root ../world_data.xml 
