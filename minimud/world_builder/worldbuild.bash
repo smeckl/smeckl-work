8850e1c21fd10b34a42af3860137655a6fa3c443
smeckl <codemunki@users.noreply.github.com>
2013-12-02 23:15:03 -0500

    Update worldbuild.bash

--- minimud/world_builder/worldbuild.bash
+++ minimud/world_builder/worldbuild.bash
@@ -2,4 +2,4 @@
 
 export CLASSPATH=/home/steve/codemunki-gitrepo/minimud/lib/mysql-connector-java-5.1.26-bin.jar:.:/home/steve/codemunki-gitrepo/minimud/minimud_shared/dist/minimud_shared.jar
 
-java -jar ./dist/world_builder.jar localhost 3306 $1 root ../world_data.xml 
+java -jar ./dist/world_builder.jar 127.0.0.1 3306 reset root ../world_data.xml 
